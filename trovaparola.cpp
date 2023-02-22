775245d0f1fec1114db5a05c5c157d78a73031ed
GianMattiaPetroni <petronigianmattia@gmail.com>
2023-02-22 15:45:31 +0100

    terza commit trovaparola

--- trovaparola.cpp
+++ trovaparola.cpp
@@ -6,11 +6,12 @@ using namespace std;
 
 int main()
 {
-
     string riga;
-    string parola = "";
+    string parola;
+    string paragrafo = "";
     int numeroRigaParolaTrovata = 0;
-    int a = 1;
+    string accomulatore;
+
     while (parola != "EXIT")
     {
         cout << "inserisci una parola da trovare" << endl;
@@ -27,43 +28,36 @@ int main()
 
         while (getline(fileInput, riga))
         {
-            if (a % 7 == 0)
+            if (numeroRigaParolaTrovata % 7 != 0)
             {
-                cout << endl;
-            }
-            else if (riga == "")
-            {
-                cout << endl;
-            }
-            else if (a < 10)
-            {
-                cout << a << "   - " << riga << endl;
-            }
-            else if (a < 100)
-            {
-                cout << a << "  - " << riga << endl;
+                accomulatore = accomulatore + riga + "\n";
             }
             else
             {
-                cout << a << " - " << riga << endl;
+                accomulatore = " ";
             }
-            a++;
-            // cout << "RIGA: " << riga << endl;
+
             numeroRigaParolaTrovata++;
             bool flag = false;                      // SE TROVO LA PAROLA IMPOSTO A TRUE
             for (int i = 0; i < riga.length(); i++) // for tutta riga
             {
-                // cout << "i: " << i << endl;
+
                 for (int j = 0; j < parola.length(); j++) // for tutta parola
                 {
-                    // cout << "j: " << j << endl;
-                    // cout << "riga[i]: " << riga[i] <<" == "<< "parola[j]:" << parola[j] << endl;
+
                     if (riga[i] == parola[j]) // se le lettere sono uguali
                     {
                         if (j == parola.length() - 1) // se le lettere sono uguali E sono all'ultima lettera della parola
                         {
                             flag = true; // PAROLA TROVATA
                             cout << "TROVATA" << endl;
+                            cout << accomulatore;
+                            
+                            while (getline(fileInput, riga)) {
+                                if(riga == " ") {
+                                    break;
+                                }
+                            }
                         }
                         i++;
                     }
@@ -83,7 +77,6 @@ int main()
                 cout << "PAROLA NON TROVATA" << endl;
             }
         }
+        fileInput.close();
     }
-
-    fileInput.close();
 }
